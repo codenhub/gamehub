13465547ce2446f6d7a3fe0d2f1e9e90a8e9dda4
Gustavo Motta <gustavohrmotta@gmail.com>
2026-01-16 22:18:47 -0300

    Changed footer heart

--- src/_ui/components/footer.ts
+++ src/_ui/components/footer.ts
@@ -6,7 +6,7 @@ class Footer extends HTMLElement {
         <div class="flex flex-col gap-4 max-w-7xl w-full py-6 mx-4">
           <div class="flex items-center justify-between">
             <p>© ${new Date().getFullYear()} Fisk. Todos os direitos reservados.</p>
-            <p>Feito com ❤︎ por <a href="https://coden.agency/">Coden</a></p>
+            <p class="flex gap-1 items-center">Feito com <img src="/assets/icons/heart.webp" alt="Heart icon" class="size-6"> por <a href="https://coden.agency/">Coden</a></p>
           </div>
         </div>
       </footer>
