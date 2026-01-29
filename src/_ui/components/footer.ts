bfae2e5652209169736e06c97dccd279f9d8a007
Gustavo Motta <gustavohrmotta@gmail.com>
2026-01-29 16:10:42 -0300

    UI changes and improvements

--- src/_ui/components/footer.ts
+++ src/_ui/components/footer.ts
@@ -6,7 +6,7 @@ class Footer extends HTMLElement {
         <div class="flex flex-col gap-4 max-w-7xl w-full py-6 mx-4">
           <div class="flex items-center justify-between">
             <p>© ${new Date().getFullYear()} GameHub. All rights reserved.</p>
-            <p class="flex gap-1 items-center">Made with <img src="/assets/icons/heart.webp" alt="Heart icon" class="size-6"> by <a href="https://coden.agency/">Coden</a></p>
+            <p class="flex gap-1 items-center">Made with <img src="/assets/icons/heart.webp" alt="Heart icon" class="size-6"> by <a href="https://coden.agency/"><img src="https://coden.agency/logo.svg" alt="Coden Agency logo" class="h-5 not-dark:brightness-0"></a></p>
           </div>
         </div>
       </footer>
