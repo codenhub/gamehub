a278bf1020b03041b1c0821cfbe5ed7093817795
Gustavo Motta <gustavohrmotta@gmail.com>
2026-02-01 17:15:29 -0300

    Added audio controls in header

--- src/_ui/components/header.ts
+++ src/_ui/components/header.ts
@@ -1,3 +1,5 @@
+import AudioManager from "../../_core/audio";
+
 const themes = {
   light: {
     "--color-primary": "var(--color-neutral-950)",
@@ -52,26 +54,70 @@ export class Header extends HTMLElement {
     setTheme(theme);
 
     this.innerHTML = `
-      <header
-        class="flex w-full justify-center p-4 border-b-2 border-border"
-      >
+      <header class="flex w-full justify-center p-4 border-b-2 border-border">
         <div class="flex max-w-7xl w-full justify-between">
           <h2 class="font-contrast">${backBtn ? `<a href="/" class="mr-6 cur-pointer"><</a>` : ""}${title}</h2>
-          <button id="theme-toggle" class="flex items-center">
-            <img
-              class="dark:hidden size-6 object-contain text-text"
-              src="/assets/icons/sun.webp"
-              alt="Sun icon"
-            >
-            <img
-              class="hidden dark:flex size-6 object-contain text-text"
-              src="/assets/icons/moon.webp"
-              alt="Moon icon"
-            >
-          </button>
+          <div class="flex gap-4 items-center">
+            <label for="sound-menu" class="relative cur-pointer">
+              <input type="checkbox" id="sound-menu" class="peer sr-only">
+              <img
+                class="size-6 object-contain text-text"
+                src="/assets/icons/volume-high.webp"
+                alt="Speaker icon"
+              >
+              <div class="hidden peer-checked:flex pointer-events-none absolute -bottom-4 right-0 translate-y-1/1 card flex-col gap-4 p-6">
+                <div class="flex items-center gap-4 w-48">
+                  <img src="/assets/icons/music.webp" alt="Music icon" class="size-6 object-contain">
+                  <input id="music-volume" type="range" min="0" max="100" value="50" class="w-full cur-default pointer-events-auto">
+                </div>
+                <div class="flex items-center gap-4 w-48">
+                  <img src="/assets/icons/volume-high.webp" alt="Sound icon" class="size-6 object-contain">
+                  <input id="sound-volume" type="range" min="0" max="100" value="75" class="w-full cur-default pointer-events-auto">
+                </div>
+              </div>
+            </label>
+            <button id="theme-toggle" class="flex items-center">
+              <img
+                class="dark:hidden size-6 object-contain text-text"
+                src="/assets/icons/sun.webp"
+                alt="Sun icon"
+              >
+              <img
+                class="hidden dark:flex size-6 object-contain text-text"
+                src="/assets/icons/moon.webp"
+                alt="Moon icon"
+              >
+            </button>
+          </div>
         </div>
       </header>
     `;
+
+    const musicVolumeEl = document.getElementById(
+      "music-volume",
+    ) as HTMLInputElement;
+    const soundVolumeEl = document.getElementById(
+      "sound-volume",
+    ) as HTMLInputElement;
+
+    const musicVolume = Number(localStorage.getItem("music-volume") || "50");
+    const soundVolume = Number(localStorage.getItem("sound-volume") || "75");
+
+    musicVolumeEl.value = musicVolume.toString();
+    soundVolumeEl.value = soundVolume.toString();
+
+    AudioManager.setMusicVolume(musicVolume / 100);
+    AudioManager.setSFXVolume(soundVolume / 100);
+
+    musicVolumeEl.addEventListener("input", () => {
+      localStorage.setItem("music-volume", musicVolumeEl.value);
+      AudioManager.setMusicVolume(musicVolumeEl.valueAsNumber / 100);
+    });
+
+    soundVolumeEl.addEventListener("input", () => {
+      localStorage.setItem("sound-volume", soundVolumeEl.value);
+      AudioManager.setSFXVolume(soundVolumeEl.valueAsNumber / 100);
+    });
 
     document.getElementById("theme-toggle")?.addEventListener("click", () => {
       if (document.documentElement.classList.contains("dark"))
